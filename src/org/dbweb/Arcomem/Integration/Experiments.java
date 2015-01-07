94056f0c1ca24619f043a38548f55aa57ff29f3e
plagree <paul.lagree@gmail.com>
2015-01-07 10:45:03 +0100

    experiment framework update

--- src/org/dbweb/Arcomem/Integration/Experiments.java
+++ src/org/dbweb/Arcomem/Integration/Experiments.java
@@ -23,7 +23,7 @@ public class Experiments {
 	public static final String taggers = "soc_tag_80";
 	private static final int k = 1000;
 	private static final int method = 1;
-	private static final int[] times = {20, 50, 100, 500, 2000};
+	private static final int[] times = {/*20,*/ 50, 200, /*500,*/ 2000};
 	private static final int lengthPrefixMinimum = 1;
 	private static double coeff = 2.0f;
 
@@ -55,7 +55,7 @@ public class Experiments {
 				0f,
 				//0.005f,
 				0.01f,
-				0.03f,
+				//0.03f,
 				//0.05f,
 				//0.07f,
 				0.1f,
